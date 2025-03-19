6b7c18c7143b2b1fab6f95d9c672f07ae3a6836b
KangJeongTaek <dfgddjd@naver.com>
2025-03-19 17:38:56 +0900

    2025-03-19 오후 5시 38분 정렬 통과 완료

---
+++ src/test/kotlin/정렬.kt
@@ -0,0 +1,28 @@
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class 정렬 {
+
+    val citations = intArrayOf(3,0,6,1,5)
+    val result = 3
+    @Test
+    fun solution()  {
+        var answer = 0
+        val sorted = citations.sorted()
+        // i  = 인용된 횟수
+        // candidate = 인용된 횟수의 논문을 포함한 보다 많이 인용된 논문의 갯수
+        run{
+            sorted.forEachIndexed { index, i ->
+                val candidate = sorted.size - index
+                if (i >= candidate) {
+                    answer =candidate
+                    return@run
+                }
+                answer = candidate
+            }
+        }
+        answer =  if (sorted.last() == 0) 0 else answer
+
+        assertEquals(result,answer)
+    }
+}
