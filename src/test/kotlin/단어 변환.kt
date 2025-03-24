16500ef3f320daf24fd71095af8a77d30dadbd16
KangJeongTaek <dfgddjd@naver.com>
2025-03-24 17:53:36 +0900

    2025-03-24 오후 5시 53분 depth 오류 수정

---
+++ src/test/kotlin/단어 변환.kt
@@ -0,0 +1,45 @@
+import org.junit.jupiter.api.*
+
+class `단어 변환` {
+
+    val begin = "hits"
+    val target = "cog"
+    val word = arrayOf("hot", "dot", "dog", "lot", "log", "cog")
+
+    @Test
+    fun solution(){
+        var answer = 0
+        if(target !in word) answer = 0
+
+        val complStringList = mutableListOf<String>()
+        val queue = ArrayDeque<Pair<String,Int>>()
+
+        queue.add(Pair(begin, 0))
+
+        while(!queue.isEmpty()) {
+            var (word1,depth) = queue.removeFirst()
+
+            if(word1 == target){
+                answer = depth
+                break
+            }
+
+            for(w in word){
+                if(w !in complStringList && isConvert(word1,w)){
+                    complStringList.add(w)
+                    queue.add(Pair(w,depth + 1))
+                }
+            }
+        }
+        return
+    }
+
+    fun isConvert(begin:String,target:String) :Boolean{
+        var result = 0
+        for(i in 0 until begin.length){
+            if(begin[i] == target[i]) result++
+        }
+
+        return result == 1
+    }
+}
