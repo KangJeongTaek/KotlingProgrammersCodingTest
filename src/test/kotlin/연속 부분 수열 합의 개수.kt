f5636315e9f291a18b2f71d009a131f9827e2ed7
KangJeongTaek <dfgddjd@naver.com>
2025-03-24 16:26:05 +0900

    2025-03-24 오후 4시 25분

---
+++ src/test/kotlin/연속 부분 수열 합의 개수.kt
@@ -0,0 +1,19 @@
+import org.junit.jupiter.api.*
+
+class `연속 부분 수열 합의 개수` {
+
+    val elements = intArrayOf(7,9,1,1,4)
+    val result = 18
+    @Test
+    fun solution(){
+        var answer : Int = 0
+        var doubleElements = elements + elements
+        val sumSet = mutableSetOf<Int>()
+        for( i in 1 ..elements.size){
+            for(j in elements.indices){
+                sumSet.add(doubleElements.slice(j until i + j).sum())
+            }
+        }
+        println(sumSet.size)
+    }
+}
---
+++ src/test/kotlin/예상_대진표.kt
@@ -0,0 +1,30 @@
+import org.junit.jupiter.api.*
+
+class 예상_대진표 {
+
+    val n = 8
+    var a = 4
+    var b = 7
+
+    //실패
+    @Test
+    fun solution(){
+        var answer = 1
+        while(Math.abs(a - b) != 1){
+            a = Math.ceil(a / 2.0).toInt()
+            b = Math.ceil(b / 2.0).toInt()
+            answer++
+        }
+        print(answer)
+    }
+
+    @Test
+    fun solution2(){
+        var answer = 0
+        while(a != b){
+            a = (a + 1) / 2
+            b = (b + 1) / 2
+            answer++
+        }
+    }
+}
